03a1a9b1f726f3e9b0a11d994edd4c3b9fbf7e4d
Christopher Marsh <christopher@ascensusglobal.com>
2025-02-02 17:19:36 -0800

    add bounce to ball

---
+++ bouncing-ball/js/1_bouncing-ball-throw.js
@@ -0,0 +1,44 @@
+const SPEED = 5;
+
+function calculateXasFunctionOfY(y) {
+  // Calculate x as a function of y
+  const x = y ** 0;
+
+  // Return value of x
+  return x;
+}
+
+// Set initial position of the ball
+let xPos = 0;
+let yPos = 0;
+
+// Add the click event handler to the ball
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('ball').addEventListener('click', throwBall);
+});
+
+// Throw the ball when we click it
+function throwBall() {
+  rise();
+}
+
+// Perform the animation when we throw the ball
+function rise() {
+  // Move the ball
+  yPos = yPos + SPEED;
+  xPos = calculateXasFunctionOfY(yPos);
+
+  const ball = document.getElementById('ball');
+
+  // Calculate the maximum height for the ball to travel upwards
+  const maxHeight =
+    document.getElementsByClassName('container')[0].offsetHeight -
+    ball.offsetHeight;
+
+  ball.style.transform = `translate(${xPos}px, -${yPos}px)`;
+
+  // Make sure that the ball does not fly into the sun
+  if (yPos <= maxHeight) {
+    requestAnimationFrame(rise);
+  }
+}
---
+++ bouncing-ball/js/2_bouncing-ball-throw-drop.js
@@ -0,0 +1,65 @@
+const SPEED = 10;
+const GRAVITY = 0.9;
+
+function calculateXasFunctionOfY(y) {
+  // Function to calculate x as a function of y
+  const x = y ** 0;
+
+  // Return x value
+  return x;
+}
+
+// Set initial position of the ball
+let xPos = 0;
+let yPos = 0;
+
+// Add the click event handler to the ball
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('ball').addEventListener('click', throwBall);
+});
+
+// Throw the ball when we click it
+function throwBall() {
+  rise();
+}
+
+// Perform the animation when we throw the ball
+function rise() {
+  // Move the ball
+  yPos = yPos + SPEED;
+  xPos = calculateXasFunctionOfY(yPos);
+
+  const ball = document.getElementById('ball');
+
+  // Calculate the maximum height for the ball to travel upwards
+  const maxHeight =
+    document.getElementsByClassName('container')[0].offsetHeight -
+    ball.offsetHeight;
+
+  // Move the ball
+  ball.style.transform = `translate(${xPos}px, -${yPos}px)`;
+
+  // Make sure that the ball does not fly into the sun
+  if (yPos >= maxHeight) {
+    requestAnimationFrame(fall);
+  } else {
+    requestAnimationFrame(rise);
+  }
+}
+
+// Perform the animation when we throw the ball
+function fall() {
+  // Move the ball
+  yPos = yPos - SPEED;
+  xPos = calculateXasFunctionOfY(yPos);
+
+  const ball = document.getElementById('ball');
+  ball.style.transform = `translate(${xPos}px, -${yPos}px)`;
+
+  // Make sure that the ball does not fly into the sun
+  if (yPos <= 0) {
+    requestAnimationFrame(rise);
+  } else {
+    requestAnimationFrame(fall);
+  }
+}
